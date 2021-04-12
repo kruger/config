f09d1451dbdce512f0aa16e58c1ce929fafe84e9
Scott Kruger <scott.e.kruger@gmail.com>
2021-04-12 13:49:17 -0600

    Bug fixes and cleanups

--- bin/rstlink.py
+++ bin/rstlink.py
@@ -4,46 +4,50 @@ import optparse
 from pathlib import Path
 import os
 
-def makeLinks(root_name):
 
-    root=Path(root_name)
+def make_links(root_name):
+    """
+    Go through root and find rst files.  Create links with the same structure
+    """
 
+    root = Path(root_name).absolute()
     if not root.exists():
         print("Does not exist: ", root_name)
         return
 
-    cur_dir=Path('.').absolute()
-    if not cur_dir.name == root.name:
+    # Always have the links in the same directory as the root name
+    # for my convenience
+    cur_dir = Path(".").absolute()
+    if cur_dir.name != root.name:
         Path(root.name).mkdir()
         os.chdir(root.name)
-        cur_dir=Path('.').absolute()
-    
-    file_entries=[]
-    for pres in root.rglob('*'):
-        sfx_match=['.rst']
-        if pres.suffix in sfx_match:
-            print(pres)
-            rel_pres=pres.relative_to(root)
-            rel_pres.parent.mkdir(parents=True, exist_ok=True)
-            rel_pres.symlink_to(cur_dir)
+        cur_dir = Path(".").absolute()
 
-    return 
+    sfx_match = [".rst"]
+    for pres in root.rglob("*"):
+        if pres.suffix in sfx_match:
+            rel_pres = pres.relative_to(root)
+            print(rel_pres)
+            rel_pres.parent.mkdir(parents=True, exist_ok=True)
+            rel_pres.symlink_to(pres)
+
+    return
 
 
 def main():
+    """  Main body:  Process arguments, call above method
+    """
     parser = optparse.OptionParser(usage="%prog [options] root")
     options, args = parser.parse_args()
 
     # Process arguments
-    if len(args) > 1:
+    if len(args) > 1 or len(args) < 1:
         parser.print_usage()
         return
-    elif len(args) == 1:
-        root = args[0]
 
-    df=makeLinks(root)
+    make_links(args[0])
     return
 
 
 if __name__ == "__main__":
-        main()
+    main()
